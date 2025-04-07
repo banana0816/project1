d8c45e40da38a6bdc91763af2519050d1098fb42
banana0816 <Zzhu@putnamscience.org>
2025-04-07 08:50:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ print("welcome to Harrison's game")
 def status(playerhp,enemyhp,playermp,enemymp): 
     print(status)
 
+
+
+
+
 def start():
     playerhp = 3
     enemyhp = 3
     playermp = 1
     enemymp = 1
+
+
+
+
 
 userinput = input("type start to start")
 if userinput.lower() == "start":
@@ -19,14 +27,39 @@ if userinput.lower() == "start":
     print("input 2 to attack")
     print("input 3 to defence")
 
+
+
+
     while True:
         choice = int(input("please type (1-3) to choose your movement:"))
 
         if choice == 1:
             playermp += 1
             print("playermp +1")
+
+
+
+
         elif choice == 2:
             while True:
                 power = int(input("choose your attack level"))
+                
+
+
+        if choice == 3:
+            while True:
+                power = int(input("choose your defence level"))
+
+            
+            
+
+
+
+
+
+
+
+
+
 
                     
